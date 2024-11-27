8ff8fdc54dcc9304c04fc9d6304c4b38631c5c63
Jc-WHI <gh30212@naver.com>
2024-11-27 23:46:21 +0900

    Initail commit

--- factorial.py
+++ factorial.py
@@ -1,14 +1,14 @@
 
 
 def fac(n:int)->int:
-    if (n==0|n==1):
+    if n==0 or n == 1:
         return 1
     else:
-        result= fac(n-1) + fac(n-2)
+        result= n*fac(n-1)
         return result 
     
 
 
-    if __name__ =='__main__':
+if __name__ =='__main__':
         result = fac(5)
         print(f"{result}")
